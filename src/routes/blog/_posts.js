1decb9aa63ed20f5dadb5352603fda6f5a412185
tamari-gray <tamari-gray>
2020-04-07 11:44:15 +1000

    new post

--- src/routes/blog/_posts.js
+++ src/routes/blog/_posts.js
@@ -1,9 +1,9 @@
 const posts = [
-	{
-		title: 'How to commit to rails',
-		slug: 'how-to-commit-to-rails',
-		date: '30/03/2020',
-		html: `
+  {
+    title: "How to commit to rails",
+    slug: "how-to-commit-to-rails",
+    date: "30/03/2020",
+    html: `
 		<p> Today I attempted and landed a 50-50 on a circle rail. It was scary and I don't think i would've done it if I didnt watch the nine club <a href="https://www.youtube.com/watch?v=A-3LUnPO5OI&t=4779s">Bastien Salabanzi</a> and <a href="https://www.youtube.com/watch?v=Aqmjl5NxhH0"> Nyjah Huston </a> interviews the past couple days. </p>
 		<p>They both shared stories of why when they go for rails, (or any scary trick) that you have to commit 100%. Any shred of doubt you have rolling up, or mid pop will only increase the chance of injury.</p>
 		<p> Nyjah shared that injuries only happen when he doesnt fully commit. And Bastien told a story of watching Tom Penny only commiting 100%, even when the board wasnt fully there.
@@ -12,13 +12,13 @@ const posts = [
 		<h2> How do you commit to rails? </h2>
 		<p> <strong>believe</strong> that if you commit, you can land it. Empty your mind of everything else </p>
 
-		`
-	},
-	{
-		title: 'How to be happy every skate',
-		slug: 'how-to-be-happy-every-skate',
-		date: '31/03/2020',
-		html: `
+		`,
+  },
+  {
+    title: "How to be happy every skate",
+    slug: "how-to-be-happy-every-skate",
+    date: "31/03/2020",
+    html: `
 		<p>
 			Have you ever been super excited to skate all day, planning and waiting, then you get to the spot or the park and you really just don't want to skate? <br>
 		</p>
@@ -37,13 +37,13 @@ const posts = [
 
 		<h2> How do you be happy every skate? </h2>
 		<p> be happy with an ollie, be happy with rolling around. Cause one day, even that might not be possible.</p>
-		`
-	},
-	{
-		title: 'How to get in the zone when your not feeling it',
-		slug: 'how-to-get-in-the-zone',
-		date: '01/04/2020',
-		html: `
+		`,
+  },
+  {
+    title: "How to get in the zone when your not feeling it",
+    slug: "how-to-get-in-the-zone",
+    date: "01/04/2020",
+    html: `
 		<p>
 			<strong> change it up! </strong>
 		</p>
@@ -59,13 +59,13 @@ const posts = [
 			To get in the zone you have to have fun. Its ok not to find every spot fun, you can always move on and find another :)
 		</p>
 		get-in-the-zone
-		`
-	},
-	{
-		title: 'How to reach new limits in skateboarding',
-		slug: 'how-to-grow',
-		date: '02/04/2020',
-		html: `
+		`,
+  },
+  {
+    title: "How to reach new limits in skateboarding",
+    slug: "how-to-grow",
+    date: "02/04/2020",
+    html: `
 		<p>
 			<strong> Find a squad </strong>
 		</p>
@@ -79,13 +79,13 @@ const posts = [
 		</p>
 
 
-		`
-	},
-	{
-		title: 'How to create magical coincidences',
-		slug: 'how-to-create-magical-coincidences',
-		date: '03/04/2020',
-		html: `
+		`,
+  },
+  {
+    title: "How to create magical coincidences",
+    slug: "how-to-create-magical-coincidences",
+    date: "03/04/2020",
+    html: `
 		<p>
 			<strong> Enjoy whats happening now, as much as you can </strong>
 		</p>
@@ -98,36 +98,59 @@ const posts = [
 			I think by really enjoying whatever is happening NOW, you allow greater opportunities to flow into your life. 
 		</p>
 
-		`
-	},
-	// template exxample post
-	// {
-	// 	title: 'How to use Sapper',
-	// 	slug: 'how-to-use-sapper',
-	// 	html: `
-	// 		<h2>Step one</h2>
-	// 		<p>Create a new project, using <a href='https://github.com/Rich-Harris/degit'>degit</at>:</p>
+		`,
+  },
+  {
+    title: "How to turn a boring skate into a fun one.",
+    slug: "how-to-turn-a-boring-skate-into-a-fun-one",
+    date: "07/04/2020",
+    html: `
+		<p>
+			<strong> Slowly build up momentum</strong>
+		</p>
 
-	// 		<pre><code>npx degit "sveltejs/sapper-template#rollup" my-app
-	// 		cd my-app
-	// 		npm install # or yarn!
-	// 		npm run dev
-	// 		</code></pre>
+    <p>
+      Today I went and skated a basketball court solo, I was having an allgood sesh then after about an hour i started to fade out and ended up just sitting down.
+      I was about to leave when i saw the curb was double the height of an average curb around the whole park, so i tried like 180 50-50, it felt really good, so i tried 180 5-0 and that felt real good too. 
+      Then i kept <strong> thinking of tricks </strong> i could do <strong>that would feel good to do.</strong>
+      And before I knew it, I was in flow state hard, I had heaps of energy, and I was having heaps of fun. 
+		</p>
 
-	// 		<h2>Step two</h2>
-	// 		<p>Go to <a href='http://localhost:3000'>localhost:3000</a>. Open <code>my-app</code> in your editor. Edit the files in the <code>src/routes</code> directory or add new ones.</p>
+    <p> 
+      I think by starting off small, doing little tricks that feel good to do, then slowly building up from there is like a gateway to energy and fun. I hope you can always have a fun skate.
+		</p>
 
-	// 		<h2>Step three</h2>
-	// 		<p>...</p>
+		`,
+  },
 
-	// 		<h2>Step four</h2>
-	// 		<p>Resist overdone joke formats.</p>
-	// 	`
-	// }
+  // template exxample post
+  // {
+  // 	title: 'How to use Sapper',
+  // 	slug: 'how-to-use-sapper',
+  // 	html: `
+  // 		<h2>Step one</h2>
+  // 		<p>Create a new project, using <a href='https://github.com/Rich-Harris/degit'>degit</at>:</p>
+
+  // 		<pre><code>npx degit "sveltejs/sapper-template#rollup" my-app
+  // 		cd my-app
+  // 		npm install # or yarn!
+  // 		npm run dev
+  // 		</code></pre>
+
+  // 		<h2>Step two</h2>
+  // 		<p>Go to <a href='http://localhost:3000'>localhost:3000</a>. Open <code>my-app</code> in your editor. Edit the files in the <code>src/routes</code> directory or add new ones.</p>
+
+  // 		<h2>Step three</h2>
+  // 		<p>...</p>
+
+  // 		<h2>Step four</h2>
+  // 		<p>Resist overdone joke formats.</p>
+  // 	`
+  // }
 ];
 
-posts.forEach(post => {
-	post.html = post.html.replace(/^\t{3}/gm, '');
+posts.forEach((post) => {
+  post.html = post.html.replace(/^\t{3}/gm, "");
 });
 
 export default posts;
